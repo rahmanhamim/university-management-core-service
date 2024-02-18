3161d8d6bd88e9a25b624d2803905728bf1874f0
rahmanhamim <rahmanhamiminfo@gmail.com>
2024-02-18 23:05:18 +0600

    get single semester done

--- src/app/modules/academicSemester/academicSemester.routes.ts
+++ src/app/modules/academicSemester/academicSemester.routes.ts
@@ -6,6 +6,9 @@ import validateRequest from '../../middlewares/validateRequest';
 import { AcademicSemesterController } from './academicSemester.controller';
 import { AcademicSemesterValidation } from './academicSemester.validation';
 
+router.get('/', AcademicSemesterController.getAllFromDB);
+router.get('/:id', AcademicSemesterController.getDataById);
+
 router.post(
   '/',
   validateRequest(AcademicSemesterValidation.create),
