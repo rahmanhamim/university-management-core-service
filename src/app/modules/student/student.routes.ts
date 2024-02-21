353f013a27c6a786f8148b782670c17a5de6ac43
rahmanhamim <rahmanhamiminfo@gmail.com>
2024-02-21 21:52:25 +0600

    delete student done

--- src/app/modules/student/student.routes.ts
+++ src/app/modules/student/student.routes.ts
@@ -10,9 +10,17 @@ router.get('/', StudentController.getAllFromDB);
 router.get('/:id', StudentController.getByIdFromDB);
 
 router.post(
-    '/',
-    validateRequest(StudentValidation.create),
-    StudentController.insertIntoDB
+  '/',
+  validateRequest(StudentValidation.create),
+  StudentController.insertIntoDB
 );
+
+router.patch(
+  '/:id',
+  validateRequest(StudentValidation.update),
+  StudentController.updateIntoDB
+);
+
+router.delete('/:id', StudentController.deleteFromDB);
 
 export const studentRoutes = router;
