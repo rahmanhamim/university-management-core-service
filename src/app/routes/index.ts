55aa239f9876a4fbc1e275f378046ec760054d62
rahmanhamim <rahmanhamiminfo@gmail.com>
2024-02-22 17:51:47 +0600

    building route added

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -2,33 +2,38 @@ import express from 'express';
 import { academicDepartmentRoutes } from '../modules/academicDepartment/academicDepartment.routes';
 import { academicFacultyRoutes } from '../modules/academicFaculty/academicFaculty.routes';
 import { AcademicSemeterRoutes } from '../modules/academicSemester/academicSemester.routes';
+import { buildingRoutes } from '../modules/building/building.routes';
 import { facultyRoutes } from '../modules/faculty/faculty.routes';
 import { studentRoutes } from '../modules/student/student.routes';
 
 const router = express.Router();
 
 const moduleRoutes = [
-  // ... routes
-  {
-    path: '/academic-semesters',
-    route: AcademicSemeterRoutes,
-  },
-  {
-    path: '/academic-faculties',
-    route: academicFacultyRoutes,
-  },
-  {
-    path: '/academic-departments',
-    route: academicDepartmentRoutes,
-  },
-  {
-    path: '/faculties',
-    route: facultyRoutes,
-  },
-  {
-    path: '/students',
-    route: studentRoutes,
-  },
+    // ... routes
+    {
+        path: '/academic-semesters',
+        route: AcademicSemeterRoutes,
+    },
+    {
+        path: '/academic-faculties',
+        route: academicFacultyRoutes,
+    },
+    {
+        path: '/academic-departments',
+        route: academicDepartmentRoutes,
+    },
+    {
+        path: '/faculties',
+        route: facultyRoutes,
+    },
+    {
+        path: '/students',
+        route: studentRoutes,
+    },
+    {
+        path: '/buildings',
+        route: buildingRoutes,
+    },
 ];
 
 moduleRoutes.forEach(route => router.use(route.path, route.route));
