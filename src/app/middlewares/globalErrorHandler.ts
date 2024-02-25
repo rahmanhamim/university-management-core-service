e7802a1ccf29a11f26b8f2e3ac5326f9335ac4b5
rahmanhamim <rahmanhamiminfo@gmail.com>
2024-02-25 15:56:03 +0600

    prisma validation added

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -6,6 +6,7 @@ import config from '../../config';
 import ApiError from '../../errors/ApiError';
 import handleValidationError from '../../errors/handleValidationError';
 
+import { Prisma } from '@prisma/client';
 import { ZodError } from 'zod';
 import handleCastError from '../../errors/handleCastError';
 import handleZodError from '../../errors/handleZodError';
@@ -13,63 +14,63 @@ import { IGenericErrorMessage } from '../../interfaces/error';
 import { errorlogger } from '../../shared/logger';
 
 const globalErrorHandler: ErrorRequestHandler = (
-  error,
-  req: Request,
-  res: Response,
-  next: NextFunction
+    error,
+    req: Request,
+    res: Response,
+    next: NextFunction
 ) => {
-  config.env === 'development'
-    ? console.log(`🐱‍🏍 globalErrorHandler ~~`, { error })
-    : errorlogger.error(`🐱‍🏍 globalErrorHandler ~~`, error);
+    config.env === 'development'
+        ? console.log(`🐱‍🏍 globalErrorHandler ~~`, { error })
+        : errorlogger.error(`🐱‍🏍 globalErrorHandler ~~`, error);
 
-  let statusCode = 500;
-  let message = 'Something went wrong !';
-  let errorMessages: IGenericErrorMessage[] = [];
+    let statusCode = 500;
+    let message = 'Something went wrong !';
+    let errorMessages: IGenericErrorMessage[] = [];
 
-  if (error?.name === 'ValidationError') {
-    const simplifiedError = handleValidationError(error);
-    statusCode = simplifiedError.statusCode;
-    message = simplifiedError.message;
-    errorMessages = simplifiedError.errorMessages;
-  } else if (error instanceof ZodError) {
-    const simplifiedError = handleZodError(error);
-    statusCode = simplifiedError.statusCode;
-    message = simplifiedError.message;
-    errorMessages = simplifiedError.errorMessages;
-  } else if (error?.name === 'CastError') {
-    const simplifiedError = handleCastError(error);
-    statusCode = simplifiedError.statusCode;
-    message = simplifiedError.message;
-    errorMessages = simplifiedError.errorMessages;
-  } else if (error instanceof ApiError) {
-    statusCode = error?.statusCode;
-    message = error.message;
-    errorMessages = error?.message
-      ? [
-          {
-            path: '',
-            message: error?.message,
-          },
-        ]
-      : [];
-  } else if (error instanceof Error) {
-    message = error?.message;
-    errorMessages = error?.message
-      ? [
-          {
-            path: '',
-            message: error?.message,
-          },
-        ]
-      : [];
-  }
+    if (error instanceof Prisma.PrismaClientValidationError) {
+        const simplifiedError = handleValidationError(error);
+        statusCode = simplifiedError.statusCode;
+        message = simplifiedError.message;
+        errorMessages = simplifiedError.errorMessages;
+    } else if (error instanceof ZodError) {
+        const simplifiedError = handleZodError(error);
+        statusCode = simplifiedError.statusCode;
+        message = simplifiedError.message;
+        errorMessages = simplifiedError.errorMessages;
+    } else if (error?.name === 'CastError') {
+        const simplifiedError = handleCastError(error);
+        statusCode = simplifiedError.statusCode;
+        message = simplifiedError.message;
+        errorMessages = simplifiedError.errorMessages;
+    } else if (error instanceof ApiError) {
+        statusCode = error?.statusCode;
+        message = error.message;
+        errorMessages = error?.message
+            ? [
+                  {
+                      path: '',
+                      message: error?.message,
+                  },
+              ]
+            : [];
+    } else if (error instanceof Error) {
+        message = error?.message;
+        errorMessages = error?.message
+            ? [
+                  {
+                      path: '',
+                      message: error?.message,
+                  },
+              ]
+            : [];
+    }
 
-  res.status(statusCode).json({
-    success: false,
-    message,
-    errorMessages,
-    stack: config.env !== 'production' ? error?.stack : undefined,
-  });
+    res.status(statusCode).json({
+        success: false,
+        message,
+        errorMessages,
+        stack: config.env !== 'production' ? error?.stack : undefined,
+    });
 };
 
 export default globalErrorHandler;
