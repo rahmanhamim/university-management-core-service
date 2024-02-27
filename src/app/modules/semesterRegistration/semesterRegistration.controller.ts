9a2201d21e33d063bfd28497c3c96962ea4ee127
rahmanhamim <rahmanhamiminfo@gmail.com>
2024-02-27 12:30:09 +0600

    update offered course class schedule

--- src/app/modules/semesterRegistration/semesterRegistration.controller.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.controller.ts
@@ -68,10 +68,24 @@ const deleteByIdFromDB = catchAsync(async (req: Request, res: Response) => {
     });
 });
 
+const startMyRegistration = catchAsync(async (req: Request, res: Response) => {
+    const user = (req as any).user;
+    const result = await SemesterRegistrationService.startMyRegistration(
+        user.userId
+    );
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Student SemesterRegistration started successfully',
+        data: result,
+    });
+});
+
 export const SemesterRegistrationController = {
     insertIntoDB,
     getAllFromDB,
     getByIdFromDB,
     deleteByIdFromDB,
     updateOneInDB,
+    startMyRegistration,
 };
