1e5662ac07ac689311c35a14feb9a69007eef462
rahmanhamim <rahmanhamiminfo@gmail.com>
2024-02-27 17:45:18 +0600

    enroll into course on progress

--- src/app/modules/semesterRegistration/semesterRegistration.controller.ts
+++ src/app/modules/semesterRegistration/semesterRegistration.controller.ts
@@ -81,6 +81,63 @@ const startMyRegistration = catchAsync(async (req: Request, res: Response) => {
     });
 });
 
+const enrollIntoCourse = catchAsync(async (req: Request, res: Response) => {
+    const user = (req as any).user;
+    const result = await SemesterRegistrationService.enrollIntoCourse(
+        user.userId,
+        req.body
+    );
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Student SemesterRegistration course enrolled successfully',
+        data: result,
+    });
+});
+
+const withdrawFromCourse = catchAsync(async (req: Request, res: Response) => {
+    const user = (req as any).user;
+    const result = await SemesterRegistrationService.withdrewFromCourse(
+        user.userId,
+        req.body
+    );
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Student Withdraw from successfully',
+        data: result,
+    });
+});
+
+const confirmMyRegistration = catchAsync(
+    async (req: Request, res: Response) => {
+        const user = (req as any).user;
+        const result = await SemesterRegistrationService.confirmMyRegistration(
+            user.userId
+        );
+        sendResponse(res, {
+            statusCode: httpStatus.OK,
+            success: true,
+            message: 'Confirm your registration!',
+            data: result,
+        });
+    }
+);
+
+const getMyRegistration = catchAsync(async (req: Request, res: Response) => {
+    const user = (req as any).user;
+    const result = await SemesterRegistrationService.getMyRegistration(
+        user.userId
+    );
+
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'My registration data fatched!',
+        data: result,
+    });
+});
+
 export const SemesterRegistrationController = {
     insertIntoDB,
     getAllFromDB,
@@ -88,4 +145,8 @@ export const SemesterRegistrationController = {
     deleteByIdFromDB,
     updateOneInDB,
     startMyRegistration,
+    enrollIntoCourse,
+    withdrawFromCourse,
+    confirmMyRegistration,
+    getMyRegistration,
 };
