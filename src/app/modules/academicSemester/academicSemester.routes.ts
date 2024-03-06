680a4c4c30e69d873949ef61dbb234b6e6cf547a
rahmanhamim <rahmanhamiminfo@gmail.com>
2024-03-06 15:24:35 +0600

    Auth added

--- src/app/modules/academicSemester/academicSemester.routes.ts
+++ src/app/modules/academicSemester/academicSemester.routes.ts
@@ -9,22 +9,23 @@ const router = express.Router();
 router.get('/', AcademicSemeterController.getAllFromDB);
 router.get('/:id', AcademicSemeterController.getDataById);
 router.post(
-  '/',
-  validateRequest(AcademicSemesterValidation.create),
-  AcademicSemeterController.insertIntoDB
+    '/',
+    validateRequest(AcademicSemesterValidation.create),
+    auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+    AcademicSemeterController.insertIntoDB
 );
 
 router.patch(
-  '/:id',
-  validateRequest(AcademicSemesterValidation.update),
-  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
-  AcademicSemeterController.updateOneInDB
+    '/:id',
+    validateRequest(AcademicSemesterValidation.update),
+    auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+    AcademicSemeterController.updateOneInDB
 );
 
 router.delete(
-  '/:id',
-  auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
-  AcademicSemeterController.deleteByIdFromDB
+    '/:id',
+    auth(ENUM_USER_ROLE.SUPER_ADMIN, ENUM_USER_ROLE.ADMIN),
+    AcademicSemeterController.deleteByIdFromDB
 );
 
 export const AcademicSemeterRoutes = router;
