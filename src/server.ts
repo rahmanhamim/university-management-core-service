756672a2d9734f395df43d37bcd03f230c0fe2a0
rahmanhamim <rahmanhamiminfo@gmail.com>
2024-03-06 14:21:35 +0600

    redis connected

--- src/server.ts
+++ src/server.ts
@@ -2,38 +2,37 @@ import { Server } from 'http';
 import app from './app';
 import config from './config';
 import { errorlogger, logger } from './shared/logger';
-
+import { RedisClient } from './shared/redis';
 
 async function bootstrap() {
+    await RedisClient.connect();
+    const server: Server = app.listen(config.port, () => {
+        logger.info(`Server running on port ${config.port}`);
+    });
 
-  const server: Server = app.listen(config.port, () => {
-    logger.info(`Server running on port ${config.port}`);
-  });
+    const exitHandler = () => {
+        if (server) {
+            server.close(() => {
+                logger.info('Server closed');
+            });
+        }
+        process.exit(1);
+    };
 
-  const exitHandler = () => {
+    const unexpectedErrorHandler = (error: unknown) => {
+        errorlogger.error(error);
+        exitHandler();
+    };
 
-    if (server) {
-      server.close(() => {
-        logger.info('Server closed');
-      });
-    }
-    process.exit(1);
-  };
+    process.on('uncaughtException', unexpectedErrorHandler);
+    process.on('unhandledRejection', unexpectedErrorHandler);
 
-  const unexpectedErrorHandler = (error: unknown) => {
-    errorlogger.error(error);
-    exitHandler();
-  };
-
-  process.on('uncaughtException', unexpectedErrorHandler);
-  process.on('unhandledRejection', unexpectedErrorHandler);
-
-  process.on('SIGTERM', () => {
-    logger.info('SIGTERM received');
-    if (server) {
-      server.close();
-    }
-  });
+    process.on('SIGTERM', () => {
+        logger.info('SIGTERM received');
+        if (server) {
+            server.close();
+        }
+    });
 }
 
 bootstrap();
